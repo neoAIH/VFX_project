d6845f7e63658375be4aedde7fac083a75fbd830
neoAIH <124498469+neoAIH@users.noreply.github.com>
2023-08-25 16:59:16 +0100

    uE5

---
+++ UE5_RPG_VFX/Source/UE5_RPG_VFX/UE5_RPG_VFX.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_RPG_VFX.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_RPG_VFX, "UE5_RPG_VFX" );
---
+++ UE5_RPG_VFX/Source/UE5_RPG_VFX/UE5_RPG_VFXGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UE5_RPG_VFXGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE5_RPG_VFX_API AUE5_RPG_VFXGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
